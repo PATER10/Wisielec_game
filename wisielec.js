485e189e0693c3633dc8023c0de6217c201422c2
PATER10 <116208725+PATER10@users.noreply.github.com>
2022-12-10 19:52:55 +0100

    randomWords

--- wisielec.js
+++ wisielec.js
@@ -1,2 +1,39 @@
-//https://colorhunt.co/palette/00000052057b892cdcbc6ff1
+let exist = false;
 let words = ["pies", "kot", "sowa", "królik", "kangur"];
+
+function randomPasswd() {
+	document.querySelector("#displayWord").innerText = "";
+	let wordsLength = words.length;
+	let random = Math.round((wordsLength - 1) * Math.random());
+	randomWord = words[random];
+	console.log(randomWord);
+	checkLetter();
+	for (let i = 1; i <= randomWord.length; i++) {
+		createNew();
+	}
+
+	return randomWord.length;
+}
+
+function createNew() {
+	const el = document.createElement("div");
+	el.className = "letters";
+	document.querySelector("#displayWord").appendChild(el);
+}
+
+function checkLetter() {
+	let letter = "a";
+	for (let j = 0; j <= 5; j++) {
+		console.log("start");
+		if (letter === randomWord[j]) {
+			let exist = true;
+			console.log(exist);
+		}
+		break;
+	}
+	if (exist === true) {
+		console.log("brawo");
+	} else {
+		console.log("Niestety");
+	}
+}
