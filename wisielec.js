65983194a1bb87f0d30e9df5d3585778e99b7607
PATER10 <116208725+PATER10@users.noreply.github.com>
2022-12-19 20:16:53 +0100

    news

--- wisielec.js
+++ wisielec.js
@@ -14,10 +14,70 @@ let words = [
 	"jastrząb",
 ];
 
+window.addEventListener("load", function () {
+	document.querySelector("#alfabet").innerHTML =
+		'<input type="button" value="Losuj hasło" id="randomPasswd" class="btn" onclick="randomPasswd()">';
+});
 function randomPasswd() {
 	finished = false;
+	litery = new Array(35);
+
+	litery[0] = "a";
+	litery[1] = "ą";
+	litery[2] = "b";
+	litery[3] = "c";
+	litery[4] = "ć";
+	litery[5] = "d";
+	litery[6] = "e";
+	litery[7] = "ę";
+	litery[8] = "f";
+	litery[9] = "g";
+	litery[10] = "h";
+	litery[11] = "i";
+	litery[12] = "j";
+	litery[13] = "k";
+	litery[14] = "l";
+	litery[15] = "ł";
+	litery[16] = "m";
+	litery[17] = "n";
+	litery[18] = "ń";
+	litery[19] = "o";
+	litery[20] = "ó";
+	litery[21] = "p";
+	litery[22] = "q";
+	litery[23] = "r";
+	litery[24] = "s";
+	litery[25] = "ś";
+	litery[26] = "t";
+	litery[27] = "u";
+	litery[28] = "v";
+	litery[29] = "w";
+	litery[30] = "x";
+	litery[31] = "y";
+	litery[32] = "z";
+	litery[33] = "ż";
+	litery[34] = "ź";
+
+	var tresc_diva = "";
+
+	for (i = 0; i <= 34; i++) {
+		var element = "lit" + i;
+		tresc_diva =
+			tresc_diva +
+			'<div class="letter" onclick="checkLetter(' +
+			i +
+			')" id="' +
+			element +
+			'">' +
+			litery[i] +
+			"</div>";
+		if ((i + 1) % 10 == 0)
+			tresc_diva = tresc_diva + '<div style="clear:both;"></div>';
+	}
+
+	document.getElementById("alfabet").innerHTML = tresc_diva;
 	document.querySelector("#btns").innerHTML =
-		'<input type="text" placeholder="Podaj literę" id="givenLetterInput" class="btn"><input type="button" value="Potwierdź" class="btn" onclick="checkLetter()"><input type="button" value="Losuj hasło" id="randomPasswd" class="btn" onclick="randomPasswd()">';
+		'<input type="button" value="Losuj hasło" id="randomPasswd" class="btn" onclick="randomPasswd()">';
 	document.querySelector("#randomPasswd").value = "Losuj nowe hasło";
 	document.querySelector("#displayWord").innerText = "";
 	document.querySelector("#wisielec_img").innerHTML =
@@ -39,9 +99,10 @@ function createNew(i) {
 	document.querySelector("#displayWord").appendChild(el);
 }
 
-function checkLetter() {
+function checkLetter(a) {
 	exist = false;
-	let letter = document.querySelector("#givenLetterInput").value;
+	let letter = litery[a];
+	console.log(letter);
 	for (let j = 0; j < randomWord.length; j++) {
 		if (letter === randomWord[j]) {
 			exist = true;
@@ -55,6 +116,7 @@ function checkLetter() {
 			'<img src="' + image + '" alt="wisielec" + id="wisielec_png" />';
 	}
 	if (fault >= 9) {
+		document.querySelector("#alfabet").innerText = "";
 		document.querySelector("#displayWord").innerText =
 			"Niestety nie udało ci się odgadnąć hasła! Prawidłowe hasło to: " +
 			randomWord;
@@ -71,6 +133,7 @@ function checkLetter() {
 		}
 	}
 	if (finished === true) {
+		document.querySelector("#alfabet").innerText = "";
 		document.querySelector("#displayWord").innerText =
 			"Brawo udało ci się odgadnąć hasło!";
 		document.querySelector("#btns").innerHTML =
